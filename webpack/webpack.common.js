07edb314538b69e3c642208030f0d0376745ac12
Leinerke Suarez <leinerke2000@gmail.com>
2022-02-22 16:50:33 -0500

    Change publicPath

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.resolve(rootDir, 'dist'),
     filename: '[name].min.js',
-    publicPath: '/',
+    publicPath: './',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
